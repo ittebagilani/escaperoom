3750ce9a16101704ce5f0b152f87c45d129d90e3
Itteba Gilani <itteba1@gmail.com>
2025-02-12 08:23:24 -0500

    final v2g

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Satisfy } from "next/font/google";
+import { Satisfy } from "next/font/google";
 
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
 const satisfy = Satisfy({weight: ["400"], subsets: ["latin"]});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Escape from the Market",
